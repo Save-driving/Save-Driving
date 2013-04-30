3da732a965b75b322e7fccc9f052ad96f73c2478
JerssonItsait <jirestrepo91@misena.edu.co>
2013-04-29 21:05:52 -0500

    Programaciones con: lazybox-willpaginate-ordenamiento de columnas

--- app/controllers/programaciones_controller.rb
+++ app/controllers/programaciones_controller.rb
@@ -1,12 +1,15 @@
 class ProgramacionesController < ApplicationController
 
+  
+  helper_method :sort_column, :sort_direction
   def index
     @rxp = (params[:numreg])? params[:numreg].to_i : 5
 
    if ((@rxp) == 0) or ((@rxp) < 0) then
       @rxp = 1
    end
-   @programaciones = Programacion.search(params[:search]).page(params[:page]).per_page(@rxp)# así queda sin hacer ordenamiento de columnas
+   @programaciones = Programacion.search(params[:search]).order(sort_column + ' ' + sort_direction).paginate(:per_page => (@rxp), :page => params[:page])
+   #@programaciones = Programacion.search(params[:search]).page(params[:page]).per_page(@rxp)# así queda sin hacer ordenamiento de columnas
    # @programaciones = Programacion.all
     respond_to do |format|
       format.html # index.html.erb
@@ -42,4 +45,14 @@ class ProgramacionesController < ApplicationController
      @programacion = Programacion.find(params[:id])
      @programacion.destroy
   end
+
+    private
+  def sort_column
+    Programacion.column_names.include?(params[:sort]) ? params[:sort] : "descripcion"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
